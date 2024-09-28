ea3cede0492e7a4ad2562b53d073af3d72660d1d
Amine Braham <spider@Legion-Aug2023>
2024-09-28 17:26:55 +0100

    initial commit

---
+++ main.js
@@ -0,0 +1,143 @@
+// create express app and serve home.html
+
+const express = require("express");
+
+const app = express();
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+var cors = require("cors");
+app.use(cors({ optionsSuccessStatus: 200 }));
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/home.html");
+});
+
+// free tier database, no persistence, don't judge me
+// yes, in a more organized project, we would use a db with proper tables, with the exercices and users seperated
+// we would also use different files for routing
+// alas, this is a simple project
+
+user_db = [];
+
+app.get("/api/users", (req, res) => {
+  res.json(user_db.map((user) => ({ _id: user._id, username: user.username })));
+});
+
+app.post("/api/users", (req, res) => {
+  const username = req.body.username;
+  if (!username) {
+    res.status(400).send("Username is required");
+    return;
+  }
+
+  for (let i = 0; i < user_db.length; i++) {
+    if (user_db[i].username === username) {
+      res.status(400).send("Username already exists");
+      return;
+    }
+  }
+
+  const id = generateId();
+  user_db.push({ _id: id, username: username, exercices: [] });
+  res.json({ _id: id, username: username });
+});
+
+app.get("/api/users/:id/logs", (req, res) => {
+  const id = req.params.id;
+  const user = user_db.find((user) => user._id === id);
+  if (!user) {
+    res.status(404).send("User not found");
+    return;
+  }
+
+  const from = req.query.from;
+  const to = req.query.to;
+  const limit = req.query.limit;
+
+  let exercices = user.exercices;
+
+  if (from) {
+    exercices = exercices.filter((exercice) => exercice.date >= from);
+  }
+
+  if (to) {
+    exercices = exercices.filter((exercice) => exercice.date <= to);
+  }
+
+  if (limit) {
+    exercices = exercices.slice(0, limit);
+  }
+
+  responseExercices = exercices.map((exercice) => ({
+    description: exercice.description,
+    duration: exercice.duration,
+    date: exercice.date.toDateString(),
+  }));
+
+  res.json({
+    _id: user._id,
+    username: user.username,
+    count: exercices.length,
+    log: responseExercices,
+  });
+});
+
+app.post("/api/users/:id/exercices", (req, res) => {
+  const id = req.params.id;
+  const user = user_db.find((user) => user._id === id);
+  if (!user) {
+    res.status(404).send("User not found");
+    return;
+  }
+
+  const description = req.body.description;
+  const duration = req.body.duration;
+  let date = req.body.date;
+
+  if (!description || !duration) {
+    res.status(400).send("Description and duration are required");
+    return;
+  }
+
+  if (typeof description !== "string") {
+    res.status(400).send("Description must be a string");
+    return;
+  }
+
+  if (isNaN(duration)) {
+    res.status(400).send("Duration must be a number");
+    return;
+  }
+
+  if (!date) {
+    date = new Date();
+  }
+
+  const exerciceObject = {
+    description: description,
+    duration: duration,
+    date: date,
+  };
+  user.exercices.push(exerciceObject);
+  res.json({
+    ...exerciceObject,
+    username: user.username,
+    _id: user._id,
+  });
+});
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Server is running on port 3000");
+});
+
+var generateId = () => {
+    var id = "";
+    var characters = "abcdefghijklmnopqrstuvwxyz0123456789";
+    for (var i = 0; i < 24; i++) {
+        id += characters.charAt(Math.floor(Math.random() * characters.length));
+    }
+    return id;
+};
